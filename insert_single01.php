f2661481cde67f5b0c5b88a56d1bfd8f4a78083b
tahrima42 <tahrima.ihsan@northsouth.edu>
2021-04-11 12:55:54 +0600

    insert_single01.php

---
+++ insert_single01.php
@@ -0,0 +1,19 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "lab04");
+// Check connection
+if($link === false){
+die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+// Attempt insert query execution
+$sql = "INSERT INTO student (First_Name, Last_Name, cgpa, semester) VALUES ('Peter', 'Parker', 3.5,4)";
+if(mysqli_query($link, $sql)){
+echo "Records inserted successfully.";
+}
+else{
+echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+// Close connection
+mysqli_close($link);
+?>
